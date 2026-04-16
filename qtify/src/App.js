32d9c130f24909823a365b856ca590e0e87d6d6b
DevShehin <shehinshajahan1998@gmail.com>
2026-04-16 13:05:01 +0530

    Trigger new deployment with followers fix

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -7,6 +7,7 @@ import { getTopAlbums, getNewAlbums } from './api/index';
 function App() {
   return (
     <div>
+      {/* QTify App - Updated with followers fix */}
        <Navbar />
         <HeroSection />
 
